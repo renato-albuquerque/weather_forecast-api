2b1793861b8bd582857630d4e3d6415cebad4593
Renato Albuquerque <renatomalb83@gmail.com>
2023-09-30 23:34:19 -0300

    new project, improvement in the first version of the project

---
+++ README.md
@@ -0,0 +1,61 @@
+# Floripa Weather Forecast | OpenWeather API
+
+Project: Development of a weather forecast app for the City of Florianópolis, Brazil.
+"Mobile First" project developed with HTML, CSS and JavaScript. Responsive page for screen sizes: 320px, 375px, 425px.
+
+Back-End challenge: 
+- [x] Consume data from the API: https://rapidapi.com/KirylBokiy/api/openweather43/
+- Provide at least two routes to the front-end: 
+- [x] Return each of the temperatures, in degrees Celsius, of the next 4 weather forecasts available for the city of “florianópolis”.
+- [x] Plus: Below each temperature, show the date/time of the weather forecast.
+- [x] Show a field with average temperatures.
+- [x] Consume other data from the API, private choice. Ex.: Wind speed, humidity, weather condition and weather description.
+
+Front-End challenge:
+- [x] Create the app interface (Display the data obtained by the API in the preferred way).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size (Developed for the breakpoints: 320px, 375px, 425px | Mobile First Project)
+- See the next 4 weather forecasts available for the city of “florianópolis” and the average
+- See wind speed, humidity, weather condition and weather description.
+
+### Screenshot
+
+#### Responsive Viewer
+![screenshot](assets/screencapture-floripa-weather-forecast-api.png)
+
+### Links
+
+- [Solution URL here](https://github.com/renato-albuquerque/floripa_weather_forecast-api)
+- [Live Site URL here](https://floripa-weather-forecast-api.vercel.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+- Mobile-first workflow
+
+## Author
+
+- Business Card - [Renato Albuquerque](https://rma-contacts.vercel.app/)
+- Linkedin - [renato-malbuquerque](https://www.linkedin.com/in/renato-malbuquerque/)
+- Discord - [Renato Albuquerque#0025](https://discordapp.com/users/992621595547938837)
---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+/*  Important information to setup the project. */
+
+/* 
+    ### sources to valid api key:
+    https://openweathermap.org/appid#example
+    https://home.openweathermap.org/api_keys
+*/
+
+/*
+    ### How can I switch between temperature units in API calls?
+    Temperature is available in Fahrenheit, Celsius and Kelvin units. Kelvin is used by default, with no need to use the units parameter in API calls.
+    For temperature in Celsius, use "units=metric". 
+    Source: https://openweathermap.org/faq#:~:text=Temperature%20is%20available%20in%20Fahrenheit,units%20parameter%20in%20API%20calls.
+*/
+
+/* 
+    ### JSON format API response fields:
+    https://openweathermap.org/current
+*/
+
+
+// variables
+
+let date = document.querySelector(".date span")
+
+let weather1 = document.querySelector(".weather-1")
+let weather2 = document.querySelector(".weather-2")
+let weather3 = document.querySelector(".weather-3")
+let weather4 = document.querySelector(".weather-4") 
+
+let time1 = document.querySelector(".time-1")
+let time2 = document.querySelector(".time-2")
+let time3 = document.querySelector(".time-3")
+let time4 = document.querySelector(".time-4")
+
+let average = document.querySelector(".average")
+
+let windSpeed = document.querySelector(".windSpeed span")
+let humidity = document.querySelector(".humidity span")
+let weatherParameter = document.querySelector(".weatherParameter span")
+let weatherDescription = document.querySelector(".weatherDescription span")
+
+let button = document.querySelector(".button") 
+
+const apiKey = "a0b6dbf50b04399ef8f5079a298d1e04"
+const cityName = "florianopolis"
+
+const url = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}&q=${cityName}&units=metric`;
+
+let todayDate = new Date();
+console.log(todayDate);
+date.textContent = `${todayDate.getDate()}/${todayDate.getMonth()+1}/${todayDate.getFullYear()}`;
+
+// events
+
+
+// functions
+
+async function weatherForecast() {
+
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+
+        weather1.textContent = `${data.list[0].main.temp.toFixed(2)}°C`;
+        weather2.textContent = `${data.list[1].main.temp.toFixed(2)}°C`;
+        weather3.textContent = `${data.list[2].main.temp.toFixed(2)}°C`;
+        weather4.textContent = `${data.list[3].main.temp.toFixed(2)}°C`;
+
+        time1.textContent = data.list[0].dt_txt;
+        time2.textContent = data.list[1].dt_txt;
+        time3.textContent = data.list[2].dt_txt;
+        time4.textContent = data.list[3].dt_txt;
+
+        average.textContent = `${((data.list[0].main.temp + data.list[1].main.temp + data.list[2].main.temp + data.list[3].main.temp)/4).toFixed(2)}°C`;
+
+        windSpeed.textContent = `${data.list[0].wind.speed.toFixed(2)} m/s`;
+        humidity.textContent = `${data.list[0].main.humidity} %`;
+        weatherParameter.textContent = data.list[0].weather[0].main;
+        weatherDescription.textContent = data.list[0].weather[0].description;
+
+    } catch (error) {
+	console.error(error);
+    }
+}
+
+weatherForecast()
+
